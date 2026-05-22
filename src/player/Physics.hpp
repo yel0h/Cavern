c2fc49ff282a168469e8369436864219faa79bf2
yello <yellowbear360@gmail.com>
2026-05-23 01:29:55 +0200

    Change physics constants

--- src/player/Physics.hpp
+++ src/player/Physics.hpp
@@ -2,12 +2,12 @@
 #define CAVERN_PHYSICS_HPP
 namespace Physics
 {
-    inline constexpr float height  = 1.7f;
-    inline constexpr float eye = 1.62f;
-    inline constexpr float width = 0.6f;
+    inline constexpr float height  = 1.75f;
+    inline constexpr float eye = 1.65f;
+    inline constexpr float width = 0.55f;
     inline constexpr float gravity = -36.f;
     inline constexpr float jump = 12.f;
-    inline constexpr float walk = 8.f;
+    inline constexpr float walk = 12.f;
     inline constexpr float airControl = 0.15f;
 }
 #endif//CAVERN_PHYSICS_HPP
