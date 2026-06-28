451a251cf25b4d604f0f2e30b350aa1275b9fc43
yello <yellowbear360@gmail.com>
2026-06-28 17:33:38 +0200

    Make placing bedrock illegal

--- src/net/Server.cpp
+++ src/net/Server.cpp
@@ -461,7 +461,6 @@ void Server::drainClients()
                         19, 20, 21, 22, 23, 24, 25, 26, 27,
                         28, 29, 30, 31, 32, 33, 34,
                         35, 36, 37, 38,
-                        8,
                 };
                 bool legalType = false;
                 for (unsigned char lb : legalBlocks)
